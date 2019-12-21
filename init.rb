1936563623eeccbc7f827a1cc58185c6520e07d6
Nguyen Ngoc Trung <nguyen.ngoc.trung@outlook.com>
2019-12-21 09:02:56 +0000

    Init plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_projects_sidebar do
+  name 'Redmine Projects Sidebar plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
